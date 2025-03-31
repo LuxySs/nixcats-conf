c695c76c0e9e73e7ace9e8a5ee0d1e213497bff4
lulu <lucas.verbeiren@gmail.com>
2025-03-31 09:39:43 +0200

    markdown-preview fix

--- lua/plugins/markdown/markdown-preview.lua
+++ lua/plugins/markdown/markdown-preview.lua
@@ -2,9 +2,8 @@ return {
   'iamcco/markdown-preview.nvim',
   enabled = require('nixCatsUtils').enableForCategory { 'markdown' },
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  build = function()
-    if not require('nixCatsUtils').isNixCats then
-      vim.fn['mkdp#util#install']()
-    end
-  end,
+  ft = { 'markdown' },
+  build = require('nixCatsUtils').lazyAdd(function()
+    vim.fn['mkdp#util#install']()
+  end),
 }
