247bd2d47a8ed0cd5d0f63a845e5fddaa80383ac
lulu <lucas.verbeiren@gmail.com>
2025-03-31 09:40:16 +0200

    minor change

--- lua/plugins/utils/neotree.lua
+++ lua/plugins/utils/neotree.lua
@@ -1,13 +1,13 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
   enabled = require('nixCatsUtils').enableForCategory { 'utils', 'neo-tree' },
-  version = '*',
+  branch = "v3.x",
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
   },
-  cmd = 'Neotree',
+  lazy = false, -- neo-tree will lazily load itself
   keys = {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   },
