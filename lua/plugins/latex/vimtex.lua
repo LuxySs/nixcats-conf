9de033c746ffbae59e9ffb31239b3fe5fe6832c0
lulu <lucas.verbeiren@gmail.com>
2025-04-21 22:46:16 +0200

    vimtex minor change

--- lua/plugins/latex/vimtex.lua
+++ lua/plugins/latex/vimtex.lua
@@ -5,6 +5,5 @@ return {
   config = function()
     vim.g.vimtex_mappings_disable = { ['n'] = { 'K' } } -- disable `K` as it conflicts with LSP hover
     vim.g.vimtex_view_method = 'zathura'
-    vim.g.vimtex_compiler_method = 'lualuatex'
   end,
 }
