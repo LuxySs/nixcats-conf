b6778bbd1d36334d233fa7bbd5720a5f39604b1a
lulu <lucas.verbeiren@gmail.com>
2025-07-05 21:06:27 +0200

    autocommands

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,10 +1,41 @@
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
+-- Basic autocommands
+local augroup_user_config = vim.api.nvim_create_augroup('UserConfig', {})
+
+-- Highlight yanked text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = augroup_user_config,
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = augroup_user_config,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
+-- Disable line numbers in terminal
+vim.api.nvim_create_autocmd('TermOpen', {
+  group = augroup_user_config,
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.opt_local.signcolumn = 'no'
+  end,
+})
+
+-- Auto-resize splits when window is resized
+vim.api.nvim_create_autocmd('VimResized', {
+  group = augroup_user_config,
+  callback = function()
+    vim.cmd('tabdo wincmd =')
   end,
 })
