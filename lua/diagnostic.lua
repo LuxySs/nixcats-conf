0cd84dcf3d71eff9d33225c05a53051ebd13d21a
lulu <lucas.verbeiren@gmail.com>
2026-02-06 19:07:29 +0100

    minor change

--- lua/diagnostic.lua
+++ lua/diagnostic.lua
@@ -22,7 +22,7 @@ map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]ui
 
 vim.diagnostic.config({
   severity_sort = true,
-  float = { border = 'rounded', source = 'if_many' },
+  float = { source = 'if_many' },
   underline = { severity = vim.diagnostic.severity.ERROR },
   signs = vim.g.have_nerd_font and {
     text = {
